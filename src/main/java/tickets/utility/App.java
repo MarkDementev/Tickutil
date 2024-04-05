e6db9894332b8fcb5562a533cf6caf4da63c72e2
MarkDementev <mark_sur@mail.ru>
2024-04-05 17:41:08 +0300

    correct utility console texts

--- src/main/java/tickets/utility/App.java
+++ src/main/java/tickets/utility/App.java
@@ -11,10 +11,10 @@ import picocli.CommandLine.Option;
 @Command(name = "tickutil", mixinStandardHelpOptions = true, version = "tickutil 1.0",
         description =
                 """
-                Utility finds 2 type of values in JSON file with array of air tickets.
+                Utility calculates 2 type of values in JSON file with array of air tickets.
                 You can add city codes to specify the calculation.
                 By default, utility works with Владивосток (VVO) and Тель-Авив (TLV) cities.
-                Finds:
+                Calculates:
                 1) Minimum flight time in minutes between cities for each air carrier.
                 2) The difference between the average and the median prices between cities.
                 """)
